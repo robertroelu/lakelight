a784244d27aafea329f9e7e5e109924c7f7a43d7
Róbert Klimant <roboklimant@gmail.com>
2024-09-20 16:50:40 +0200

    chore: adding scripts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 //Finsweet attributes
 import { linkblockedit } from '@finsweet/attributes-linkblockedit';
 // import { } from '@finsweet/attributes-cmssort/'
-// import { } from '@finsweet/attributes-cmsfilter/'
+import { cmsfilter } from '@finsweet/attributes-cmsfilter';
 // import { } from '@finsweet/attributes-scrolldisable/'
-// import { } from '@finsweet/attributes-cmsload/'
+import { cmsload } from '@finsweet/attributes-cmsload/';
 // import { } from '@finsweet/attributes-socialshare/'
 
 //Modal
@@ -14,6 +14,14 @@ import { linkblockedit } from '@finsweet/attributes-linkblockedit';
 
 //Utils
 import { swipers } from '$utils/swipers';
+import { searchLoad } from '$utils/searchLoad';
+import { socialShare } from '$utils/socialShare';
+import { searchFind } from '$utils/searchFind';
+import { breadCrumb } from '$utils/breadCrumbs';
+import { speakersLoad } from '$utils/speakersLoad';
+import { scrollAnimation } from '$utils/scrollAnimation';
+import { backStep } from '$utils/backStep';
+import { hoverAnimation } from '$utils/hoverAnimation';
 // import { consoleClear } from '$utils/consoleClear';
 // import { actualyear } from '$utils/actualYear';
 // import { typer } from '$utils/typer';
@@ -24,7 +32,24 @@ window.Webflow.push(() => {
   // actualyear();
   // nestedElement();
   linkblockedit();
+  cmsload();
   // modal();
   swipers();
+
+  searchLoad();
+  cmsfilter();
+  searchFind();
+
+  socialShare();
+
+  breadCrumb();
+
+  speakersLoad();
+
+  scrollAnimation();
+
+  backStep();
+
+  hoverAnimation();
   // typer();
 });
