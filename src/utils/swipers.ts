163a088d99879386c3c0e82fc8ef975b052fc311
Róbert Klimant <roboklimant@gmail.com>
2024-09-20 19:56:13 +0200

    fix: adding responsivity

--- src/utils/swipers.ts
+++ src/utils/swipers.ts
@@ -1,5 +1,58 @@
-import 'swiper/css/bundle';
+// import 'swiper/css/bundle';
 
+// import Swiper from 'swiper';
+// import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+
+// export const swipers = () => {
+//   const swiperModules = [Autoplay, EffectFade, Pagination, Navigation];
+
+//   const swipersEl = document.querySelectorAll('[swiper-option]') as NodeListOf<HTMLElement>;
+//   if (!swipersEl) return;
+
+//   swipersEl.forEach((el) => {
+//     const attrEl = el.getAttribute('swiper-option');
+
+//     if (attrEl === 'home-articles') {
+//       const elPrev = document.querySelector(`[swiper-prev="${attrEl}"]`) as HTMLElement;
+//       const elNext = document.querySelector(`[swiper-next="${attrEl}"]`) as HTMLElement;
+
+//       let swiper = new Swiper(el, {
+//         modules: swiperModules,
+//         speed: 500,
+//         slidesPerView: 3.3,
+//         allowTouchMove: true,
+//         // rewind: true,
+//         spaceBetween: 32,
+//         breakpoints: {
+//           480: {
+//             slidesPerView: 1,
+//             spaceBetween: 20,
+//           },
+//           767: {
+//             slidesPerView: 3,
+//             spaceBetween: 30,
+//           },
+//           991: {
+//             slidesPerView: 4,
+//             spaceBetween: 40,
+//           },
+//         },
+//         navigation: {
+//           nextEl: elNext,
+//           prevEl: elPrev,
+//         },
+//         pagination: {
+//           el: '.home_articles_nav-bullet-wrap',
+//           bulletClass: 'home_articles_nav-bullet',
+//           bulletActiveClass: 'home_articles_nav-bullet-active',
+//           clickable: true,
+//         },
+//       });
+//     }
+//   });
+// };
+
+import 'swiper/css/bundle';
 import Swiper from 'swiper';
 import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
 
@@ -21,11 +74,27 @@ export const swipers = () => {
         speed: 500,
         slidesPerView: 3.3,
         allowTouchMove: true,
-        // rewind: true,
         spaceBetween: 32,
         navigation: {
           nextEl: elNext,
           prevEl: elPrev,
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 1.1,
+          },
+          480: {
+            slidesPerView: 1.6,
+          },
+          767: {
+            slidesPerView: 2.1,
+          },
+          991: {
+            slidesPerView: 2.3,
+          },
+          1100: {
+            slidesPerView: 3.3,
+          },
         },
         pagination: {
           el: '.home_articles_nav-bullet-wrap',
