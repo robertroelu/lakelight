24de46dfc70ce9c799d882492828c71d2d9ddfba
Róbert Klimant <roboklimant@gmail.com>
2024-10-04 18:39:50 +0200

    fix: merge two arrays for tags

--- src/utils/searchLoad.ts
+++ src/utils/searchLoad.ts
@@ -1,12 +1,67 @@
+// export const searchLoad = () => {
+//   const elPrimary = document.querySelector('[search-main]') as HTMLElement;
+//   if (!elPrimary) return;
+//   const elSecondary = document.querySelector('[search-secondary]') as HTMLElement;
+//   if (!elSecondary) return;
+
+//   // const elements = elSecondary.querySelectorAll('[role="listitem"]') as NodeListOf<HTMLElement>;
+//   const elements = elSecondary.querySelectorAll(
+//     '.search_collection-item'
+//   ) as NodeListOf<HTMLElement>;
+
+//   elements.forEach((element) => {
+//     elPrimary.appendChild(element);
+//   });
+// };
+
 export const searchLoad = () => {
   const elPrimary = document.querySelector('[search-main]') as HTMLElement;
   if (!elPrimary) return;
-  const elSecondary = document.querySelector('[search-secondary]') as HTMLElement;
+  const elSecondary = document.querySelectorAll('[search-secondary]') as NodeListOf<HTMLElement>;
   if (!elSecondary) return;
 
-  const elements = elSecondary.querySelectorAll('[role="listitem"]') as NodeListOf<HTMLElement>;
+  elSecondary.forEach((secondEl) => {
+    // const elements = elSecondary.querySelectorAll('[role="listitem"]') as NodeListOf<HTMLElement>;
+    const elements = secondEl.querySelectorAll(
+      '.search_collection-item'
+    ) as NodeListOf<HTMLElement>;
 
-  elements.forEach((element) => {
-    elPrimary.appendChild(element);
+    elements.forEach((element) => {
+      elPrimary.appendChild(element);
+    });
+  });
+
+  const eventItem = elPrimary.querySelectorAll('[event-slug]') as NodeListOf<HTMLElement>;
+  const eventItemTags = elPrimary.querySelectorAll('[event-slug-tags]') as NodeListOf<HTMLElement>;
+
+  eventItem.forEach((item) => {
+    const slug = item.getAttribute('event-slug');
+
+    eventItemTags.forEach((itemTags) => {
+      const slugTag = itemTags.getAttribute('event-slug-tags');
+      if (slug === slugTag) {
+        const nestEl = itemTags.querySelector('.search_collection-nest-wrapper') as HTMLElement;
+        item.append(nestEl);
+        itemTags.remove();
+      }
+    });
+  });
+
+  const listenItem = elPrimary.querySelectorAll('[listen-slug]') as NodeListOf<HTMLElement>;
+  const listenItemTags = elPrimary.querySelectorAll(
+    '[listen-slug-tags]'
+  ) as NodeListOf<HTMLElement>;
+
+  listenItem.forEach((item) => {
+    const slug = item.getAttribute('listen-slug');
+
+    listenItemTags.forEach((itemTags) => {
+      const slugTag = itemTags.getAttribute('listen-slug-tags');
+      if (slug === slugTag) {
+        const nestEl = itemTags.querySelector('.search_collection-nest-wrapper') as HTMLElement;
+        item.append(nestEl);
+        itemTags.remove();
+      }
+    });
   });
 };
